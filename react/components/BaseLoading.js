50f46deac145f7153e8fce2f0f5997729ad8f41b
Felipe Sales <sales.felipeb@gmail.com>
2018-11-19 17:57:33 -0200

    Add namespace to the BaseLoading component

--- react/components/BaseLoading.js
+++ react/components/BaseLoading.js
@@ -32,7 +32,7 @@ class BaseLoading extends Component {
       queryData.error.toString().indexOf('not authenticated') > -1
 
     return (
-      <ContentWrapper {...headerConfig}>
+      <ContentWrapper namespace="vtex.base-loading" {...headerConfig}>
         {() => (
           <Fragment>
             {isLoading ? (
