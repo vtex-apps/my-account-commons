0d590c2ecc82156750763de0b3ae2add78417690
Felipe Sales <sales.felipeb@gmail.com>
2018-11-27 17:54:51 -0200

    Add fixImageUrl function

--- react/utils.js
+++ react/utils.js
@@ -62,6 +62,67 @@ export function reduceBundleItems(items) {
   )
 }
 
+export function fixImageUrl(
+  imageUrl,
+  width = DEFAULT_WIDTH,
+  height = DEFAULT_HEIGHT
+) {
+  return changeImageUrlSize(
+    replaceHttpToRelativeProtocol(cleanImageUrl(imageUrl)),
+    width,
+    height
+  )
+}
+
+const DEFAULT_WIDTH = 50
+const DEFAULT_HEIGHT = 50
+const DEFAULT_HDF = 1
+
+const baseUrlRegex = new RegExp(/.+ids\/(\d+)(?:-(\d+)-(\d+)|)\//)
+const sizeRegex = new RegExp(/-(\d+)-(\d+)/)
+
+function changeImageUrlSize(
+  imageUrl,
+  width,
+  height,
+  highDensityFactor = DEFAULT_HDF
+) {
+  // imageUrl example http://omniera.vteximg.com.br/arquivos/ids/155401-135-135/CAN-09-04--1-.jpg
+  if (!imageUrl || !width || !height) return undefined
+  const widthCalc = width * highDensityFactor
+  const heightCalc = height * highDensityFactor
+  if (imageUrl.slice(-1) !== '/') {
+    return `${imageUrl}?width=${widthCalc}&height=${heightCalc}&aspect=true`
+  }
+  const resizedImageUrl = imageUrl.slice(0, -1) // Remove last "/"
+  return `${resizedImageUrl}-${widthCalc}-${heightCalc}`
+}
+
+function replaceHttpToRelativeProtocol(url) {
+  if (!url) {
+    return undefined
+  }
+  return url.replace(/https:\/\/|http:\/\//, '//')
+}
+
+function cleanImageUrl(imageUrl) {
+  let resizedImageUrl = imageUrl
+  const result = baseUrlRegex.exec(imageUrl)
+  if (result && result.length > 0) {
+    if (
+      result.length === 4 &&
+      result[2] !== undefined &&
+      result[3] !== undefined
+    ) {
+      resizedImageUrl = result[0].replace(sizeRegex, '')
+    } else {
+      resizedImageUrl = result[0]
+    }
+    return resizedImageUrl
+  }
+  return resizedImageUrl
+}
+
 function checkForAttachments(item) {
   if (item.attachments && item.attachments.length > 0) {
     return item.attachments.map(addAttachmentFlag)
