d31932b4476e368615a4fbce6cc0d1d41c0ca2e3
Felipe Sales <sales.felipeb@gmail.com>
2018-12-06 16:17:20 -0200

    Fix PackageProgress exportation

--- react/ProgressBarBundle.js
+++ react/ProgressBarBundle.js
@@ -10,6 +10,7 @@ import OrderStatus from './components/ProgressBar/OrderStatus'
 import ProgressBar from './components/ProgressBar/ProgressBar'
 import ProgressBarSection from './components/ProgressBar/ProgressBarSection'
 import PackageStatus from './components/ProgressBar/PackageProgressBar/PackageStatus'
+import PackageProgressBar from './components/ProgressBar/PackageProgressBar/PackageProgressBar'
 import PackageProgressBarSection from './components/ProgressBar/PackageProgressBar/PackageProgressBarSection'
 
 const constants = {
@@ -26,8 +27,9 @@ export default {
   constants,
   utils,
   OrderStatus,
+  ProgressBar,
   ProgressBarSection,
+  PackageProgressBar,
   PackageProgressBarSection,
   PackageStatus,
-  ProgressBar,
 }
