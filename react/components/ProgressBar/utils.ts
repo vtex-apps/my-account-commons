e75dd3efbe64f7612cb16d130ff99b5e5b950509
Bianca Villar <biancadfvillar@gmail.com>
2020-06-29 14:44:22 -0300

    Fix timeline for pickup orders

--- react/components/ProgressBar/utils.ts
+++ react/components/ProgressBar/utils.ts
@@ -1,36 +1,41 @@
-import { FIFTH_STEP, THIRD_STEP } from './constants'
+import { FIFTH_STEP, THIRD_STEP, FOURTH_STEP } from './constants'
 
 export function generateProgressBarStates(
-  states: any[],
+  progressBarStates: any[],
   currentState: number,
   packages: any
 ) {
-  return states.map((state, index) => {
-    let label = null
-    if (
-      index === FIFTH_STEP &&
-      currentState === FIFTH_STEP &&
-      isDelivered(packages)
-    ) {
+  const wasDelivered = isDelivered(packages)
+  const isPickup = isOrderPickUp(packages)
+
+  const states = progressBarStates.map((state, index) => {
+    let label = state.todo
+
+    if (isPickup && index == FOURTH_STEP) {
+      if (index > currentState) {
+        label = state.todo_pickup
+      } else if (wasDelivered) {
+        label = state.done_pickup
+      } else {
+        label = state.doing_pickup
+      }
+    } else if (index < currentState) {
       label = state.done
-    } else if (
-      index === FIFTH_STEP &&
-      currentState === FIFTH_STEP &&
-      isOrderReadyToPickUp(packages)
-    ) {
-      label = state.doing_pickup
-    } else if (currentState === index) {
-      label = state.doing
-    } else if (currentState > index) {
-      label = state.done
-    } else {
-      label = state.todo
+    } else if (index == currentState) {
+      if (index == FIFTH_STEP && wasDelivered) {
+        label = state.done
+      } else {
+        label = state.doing
+      }
     }
+
     return {
       ...state,
       label: label,
     }
   })
+
+  return isPickup ? states.splice(0, 4) : states
 }
 
 export function isDelivered(packages: any) {
@@ -89,4 +94,10 @@ export function isOrderReadyToPickUp(packages: any) {
       Date.now() >= new Date(item.shippingEstimateDate).getTime()
     return isPickUp && isReadyToPickUp
   })
+}
+
+export function isOrderPickUp(packages: any) {
+  if (packages == null || packages.length === 0) return false
+
+  return packages.every((pck: any) => pck.deliveryChannel === 'pickup-in-point')
 }
